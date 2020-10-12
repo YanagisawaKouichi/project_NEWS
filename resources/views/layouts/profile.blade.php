d1f544fbe695af3bb542e84660d2bca6fb41568a
yanagisawa <kou0424kou@icloud.com>
2020-10-12 14:45:24 +0000

    PHP/Laravel16の課題です

--- resources/views/layouts/profile.blade.php
+++ resources/views/layouts/profile.blade.php
@@ -17,19 +17,18 @@
         <link rel="dns-prefetch" href="https://fonts.gstatic.com">
         <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
         
-        <!-- styles -->
+        <!--Styles -->
+        <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
         <link href="{{ secure_asset('css/profile.css') }}" rel="stylesheet">
     </head>
     <body>
         <div id="app">
-            <nav class="navbar navbar-expabd-md navbar-dark navbar-laravel">
+            <nav class="navbar navbar-expand-md navbar-dark navbar-laravel">
                 <div class="container">
-                    <a class="navbar-brabd" href="{{ url('/') }}">
+                    <a class="navbar-brand" href="{{ url('/') }}">
                         {{ config('app.name', 'Laravel') }}
                     </a>
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-
-                    target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-ezpanded="false"
-                    aria-label="Toggle navigation">
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-ezpanded="false"aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
                     
